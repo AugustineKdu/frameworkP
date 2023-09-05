49f6878b0687ea01f9f3f3c854275d396101a926
Augustine <augustine.kim@griffithuni.edu.au>
2023-09-05 20:26:56 +1000

    before connect nodejs

--- frontend/src/app/account/account.component.ts
+++ frontend/src/app/account/account.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css']
 })
-export class AccountComponent {
+export class AccountComponent implements OnInit {
+  user: any;
 
+  ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('currentUser') || '{}');
+  }
 }
