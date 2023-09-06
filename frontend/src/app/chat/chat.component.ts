004124695894205aae51b7737f031abfeff7584b
Augustine <augustine.kim@griffithuni.edu.au>
2023-09-07 05:39:23 +1000

    add the chat component ts to connect with local json file and documentation on code

--- frontend/src/app/chat/chat.component.ts
+++ frontend/src/app/chat/chat.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
 
+// Define the structure of a chat group
 interface ChatGroup {
   id: number;
   name: string;
@@ -18,95 +19,95 @@ export class ChatComponent implements OnInit {
   newMessage = '';
   chatGroups: ChatGroup[] = [];
   selectedGroup: ChatGroup | null = null;
-  currentUser: any = null; // 현재 사용자 정보를 저장할 변수
+  currentUser: any = null; // To store the current user's information
 
   constructor(private http: HttpClient) {
+    // Initialize Socket.io
     this.socket = io('http://localhost:3000');
 
+    // Listen for new messages and update the UI
     this.socket.on('new message', (data: { roomId: number; message: string }) => {
       if (this.selectedGroup?.id === data.roomId) {
         this.selectedGroup.messages.push({ content: data.message });
-        this.saveToLocalStorage();
+        this.saveToLocalStorage(); // Save updated messages to local storage
       }
     });
   }
 
   ngOnInit(): void {
-    // Load from local storage first
+    // Load chat groups from local storage first
     this.chatGroups = this.loadFromLocalStorage();
 
-    // Then update from server
+    // Then update from the server
     this.http.get<ChatGroup[]>('http://localhost:3000/api/chat-groups').subscribe((groups) => {
       this.chatGroups = groups;
-      this.saveToLocalStorage();
+      this.saveToLocalStorage(); // Save to local storage
     });
 
-    // Load current user from session storage
+    // Load the current user from session storage
     const user = sessionStorage.getItem('currentUser');
     if (user) {
       this.currentUser = JSON.parse(user);
     }
   }
 
+  // Function to select a chat group
   selectGroup(groupId: number): void {
     this.selectedGroup = this.chatGroups.find((group) => group.id === groupId) || null;
     if (this.selectedGroup) {
-      this.socket.emit('joinRoom', groupId);
+      this.socket.emit('joinRoom', groupId); // Join the selected chat room
     }
   }
+
+  // Function to add a new chat group
   addGroup() {
-    if (this.currentUser) {
-      if (this.currentUser.role === 'group-admin' || this.currentUser.role === 'super-admin') {
-        const groupName = prompt('Enter new group name:');
-        if (groupName) {
-          this.http.post<ChatGroup>('http://localhost:3000/api/chat-groups', { name: groupName }).subscribe((newGroup) => {
-            this.chatGroups.push(newGroup);
-            this.saveToLocalStorage();
-          });
-        }
-      } else {
-        alert('You do not have permission to add a group.');
-      }
-    } else {
-      alert('You are not logged in.');
-    }
-  }
-
-  deleteGroup(groupId: number) {
-    if (this.currentUser) {
-      if (this.currentUser.role === 'group-admin' || this.currentUser.role === 'super-admin') {
-        this.http.delete(`http://localhost:3000/api/chat-groups/${groupId}`).subscribe(() => {
-          const index = this.chatGroups.findIndex(group => group.id === groupId);
-          if (index !== -1) {
-            this.chatGroups.splice(index, 1);
-            this.saveToLocalStorage();
-          }
+    // Check if the user has the permission to add a group
+    if (this.currentUser && (this.currentUser.role === 'group-admin' || this.currentUser.role === 'super-admin')) {
+      const groupName = prompt('Enter new group name:');
+      if (groupName) {
+        this.http.post<ChatGroup>('http://localhost:3000/api/chat-groups', { name: groupName }).subscribe((newGroup) => {
+          this.chatGroups.push(newGroup);
+          this.saveToLocalStorage(); // Save to local storage
         });
-      } else {
-        alert('You do not have permission to delete a group.');
       }
     } else {
-      alert('You are not logged in.');
+      alert('You do not have permission to add a group.');
     }
   }
 
+  // Function to delete a chat group
+  deleteGroup(groupId: number) {
+    // Check if the user has the permission to delete a group
+    if (this.currentUser && (this.currentUser.role === 'group-admin' || this.currentUser.role === 'super-admin')) {
+      this.http.delete(`http://localhost:3000/api/chat-groups/${groupId}`).subscribe(() => {
+        const index = this.chatGroups.findIndex(group => group.id === groupId);
+        if (index !== -1) {
+          this.chatGroups.splice(index, 1);
+          this.saveToLocalStorage(); // Save to local storage
+        }
+      });
+    } else {
+      alert('You do not have permission to delete a group.');
+    }
+  }
 
+  // Function to send a message
   sendMessage(): void {
-    if (this.currentUser) {
-      if (this.selectedGroup) {
-        this.socket.emit('send message', { message: this.newMessage, roomId: this.selectedGroup.id });
-        this.newMessage = '';
-      }
+    // Check if the user is logged in
+    if (this.currentUser && this.selectedGroup) {
+      this.socket.emit('send message', { message: this.newMessage, roomId: this.selectedGroup.id });
+      this.newMessage = ''; // Clear the input field
     } else {
       alert('You must be logged in to send a message.');
     }
   }
 
-
+  // Function to save chat groups to local storage
   saveToLocalStorage(): void {
     localStorage.setItem('chatGroups', JSON.stringify(this.chatGroups));
   }
 
+  // Function to load chat groups from local storage
   loadFromLocalStorage(): ChatGroup[] {
     const storedGroups = localStorage.getItem('chatGroups');
     return storedGroups ? JSON.parse(storedGroups) : [];
